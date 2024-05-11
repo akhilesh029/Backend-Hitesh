372e2626ecdb44fb75aa92f6fd87895946ca31ad
akhilesh029 <akhilesheka0100@gmail.com>
2024-05-11 16:03:00 +0530

    connect to mongoAtlas

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// require('dotenv').config({path: './env'})
+
+
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants";
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env"
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+/*
+import express from 'express'
+const app = express()
+( async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}`)
+        app.on("error", (error)=>{
+           console.log('Not able to talk database');
+           throw error
+           
+            
+        })
+
+        app.listen(process.env.PORT , () =>{
+            console.log(`app is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw error
+    }
+} ) ()
+
+*/
